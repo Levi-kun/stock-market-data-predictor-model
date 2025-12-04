7956fe1702732ad2ce826b1c1e73a74b6607d79f
eduardo <ep3700299@gmail.com>
2025-12-04 14:12:02 -0500

    update css

--- Stock-Market-Web-App/config.py
+++ Stock-Market-Web-App/config.py
@@ -1,18 +1,15 @@
-import os 
-from pathlib import Path 
+import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
 
 class Config:
-    ## Get the project base directory 
-    BASE_DIR = Path(__file__).resolve().parent
-
     SECRET_KEY = os.getenv("SECRET_KEY")
 
     DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = int(os.getenv("DB_PORT"))
+    DB_PORT = int(os.getenv("DB_PORT","5432"))  # fallback default
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_USER = os.getenv("DB_USER")
     DB_NAME = os.getenv("DB_NAME")
-
