686ac4eceeedc8e894930501c217aabbcb694161
bocha-bib <rmunozgarcia005@gmail.com>
2025-12-09 19:59:30 -0500

    Update login.py

--- Stock-Market-Web-App/app/login.py
+++ Stock-Market-Web-App/app/login.py
@@ -1,2 +1,20 @@
-def handle_login():
-    print("Hello!")
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
