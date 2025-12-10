18b024de4c496da1a8b476fe42b3cb932d51e0f4
bocha-bib <rmunozgarcia005@gmail.com>
2025-12-09 20:01:47 -0500

    Update forms.py

--- Stock-Market-Web-App/app/forms.py
+++ Stock-Market-Web-App/app/forms.py
@@ -1,1 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+import sqlalchemy as sa
+from app import db
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_repeat = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')]
+    )
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
