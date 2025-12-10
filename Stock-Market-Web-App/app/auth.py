771951d2f80b43f8afe5c36b955c87ea19408abf
bocha-bib <rmunozgarcia005@gmail.com>
2025-12-09 20:00:54 -0500

    Update auth.py

--- Stock-Market-Web-App/app/auth.py
+++ Stock-Market-Web-App/app/auth.py
@@ -1,1 +1,29 @@
+from flask import Blueprint, redirect, render_template, flash, request, session, url_for
+from flask_login import login_required, logout_user, current_user, login_user
+from .forms import LoginForm, SignupForm
+from .models import db, User
 
+@auth_bp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    
+    form = SignupForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+        if existing_user is None:
+            user = User(
+                name=form.name.data,
+                email=form.email.data
+                )
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            return redirect(url_for('main_bp.dashboard'))
+        flash('A user already exists with that email address.')
+    return render_template(
+        'signup.jinja2',
+        title='Create an Account.',
+        form=form,
+        template='signup-page',
+        body="Sign up for a user account."
+    )
